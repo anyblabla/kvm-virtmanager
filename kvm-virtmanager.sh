9d8ff7af5c4d9eb44ed31a604771bc8ac62f06ca
Amaury Libert <blablalinux@outlook.be>
2021-12-27 14:42:31 +0100

    Update kvm-virtmanager.sh
    Ajout du paquet "ovmf" pour pouvoir utiliser le firmware de type UEFI

--- kvm-virtmanager.sh
+++ kvm-virtmanager.sh
@@ -21,9 +21,9 @@ clear
 echo "Rafraîchissement dépôts..."
 apt update
 #
-echo "Installations de qemu, qemu-kvm, libvirt0, virt-manager, libguestfs-tools, ssh-askpass, bridge-utils, gnome-boxes..."
+echo "Installations de qemu, qemu-kvm, libvirt0, virt-manager, libguestfs-tools, ovmf, ssh-askpass, bridge-utils, gnome-boxes..."
 apt install -y -o 'apt::install-recommends=true' \
-  qemu qemu-kvm libvirt0 virt-manager libguestfs-tools ssh-askpass bridge-utils gnome-boxes ssh-askpass-gnome openssh-server
+  qemu qemu-kvm libvirt0 virt-manager libguestfs-tools ovmf ssh-askpass bridge-utils gnome-boxes ssh-askpass-gnome openssh-server
 #
 echo "KVM ajouts groupes..."
 usermod -G kvm -a $SUDO_USER
